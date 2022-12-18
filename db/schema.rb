0c74567a48882f44e78d1ff8b24e126c9b94b3fc
EC2 Default User <ec2-user@ip-172-31-18-54.ap-northeast-1.compute.internal>
2022-12-18 12:25:14 +0000

    非同期通信

--- db/schema.rb
+++ db/schema.rb
@@ -107,6 +107,8 @@ ActiveRecord::Schema.define(version: 2022_12_15_040618) do
     t.integer "visited_id", null: false
     t.integer "post_id"
     t.integer "comment_id"
+    t.integer "chat_room_id"
+    t.integer "chat_id"
     t.string "action", default: "", null: false
     t.boolean "checked", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
