aff893177b390a188cb4652693c648adacb2683f
EC2 Default User <ec2-user@ip-172-31-18-54.ap-northeast-1.compute.internal>
2022-12-19 14:42:59 +0000

    layout修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,9 +11,39 @@ import "channels"
 import "jquery";
 import "popper.js";
 import "bootstrap";
-import "../stylesheets/application" 
+import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// ===========ヘッダーメニュー=============
+/*global $*/
+//ドロップダウンの設定を関数でまとめる
+function mediaQueriesWin(){
+  var width = $(window).width();
+  if(width <= 768) {//横幅が768px以下の場合
+    $(".has-child>a").off('click'); //has-childクラスがついたaタグのonイベントを複数登録を避ける為offにして一旦初期状態へ
+    $(".has-child>a").on('click', function() {//has-childクラスがついたaタグをクリックしたら
+      var parentElem =  $(this).parent();// aタグから見た親要素の<li>を取得し
+      $(parentElem).toggleClass('active');//矢印方向を変えるためのクラス名を付与して
+      $(parentElem).children('ul').stop().slideToggle(500);//liの子要素のスライドを開閉させる※数字が大きくなるほどゆっくり開く
+      return false;//リンクの無効化
+    });
+  }else{//横幅が768px以上の場合
+    $(".has-child>a").off('click');//has-childクラスがついたaタグのonイベントをoff(無効)にし
+    $(".has-child").removeClass('active');//activeクラスを削除
+    $('.has-child').children('ul').css("display","");//スライドトグルで動作したdisplayも無効化にする
+  }
+}
+
+// ページがリサイズされたら動かしたい場合の記述
+$(window).resize(function() {
+  mediaQueriesWin();/* ドロップダウンの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load',function(){
+  mediaQueriesWin();/* ドロップダウンの関数を呼ぶ*/
+});
