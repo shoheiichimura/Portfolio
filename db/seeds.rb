6c82e2c2cbad47cf312922ed3730f598a92c7a7d
EC2 Default User <ec2-user@ip-172-31-18-54.ap-northeast-1.compute.internal>
2023-01-15 06:26:14 +0000

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,70 +13,70 @@
 
 # =======管理者情報======
 
-# Admin.create!(
-#   email: 'a@a',
-#   password: 'adminadmin',
-#   )
+Admin.create!(
+  email: 'a@a',
+  password: 'adminadmin',
+  )
 
 
 
 #========男性会員情報作成=========
 
-# for n in 1..10
-#   ActiveRecord::Base.transaction do
-#       customer = Customer.new(
-#         password: SecureRandom.urlsafe_base64,
-#         email: "test_man#{n}@test.com",
-#         name: "test男性#{n}",
-#         introduction: "よろしくお願いします",
-#         sex: 0,
-#         history: n % 4,
-#         active_area: n % 47,
-#         objective: n % 5,
-#         frequency: n % 4,
-#         heart: n % 3,
-#         traning_style: n % 4
-#         )
-#       customer.profile_image.attach(io: File.open("./db/fixtures/sample-man#{n}.jpg"), filename: "sample-man#{n}.jpg", content_type: 'image/jpg')
-#       customer.save!
-#   end
-# end
+for n in 1..10
+  ActiveRecord::Base.transaction do
+      customer = Customer.new(
+        password: SecureRandom.urlsafe_base64,
+        email: "test_man#{n}@test.com",
+        name: "test男性#{n}",
+        introduction: "よろしくお願いします",
+        sex: 0,
+        history: n % 4,
+        active_area: n % 47,
+        objective: n % 5,
+        frequency: n % 4,
+        heart: n % 3,
+        traning_style: n % 4
+        )
+      customer.profile_image.attach(io: File.open("./db/fixtures/sample-man#{n}.jpg"), filename: "sample-man#{n}.jpg", content_type: 'image/jpg')
+      customer.save!
+  end
+end
 
 # # ========女性会員情報作成=========
 
-# for n in 1..10
-#   ActiveRecord::Base.transaction do
-#       customer = Customer.new(
-#         password: SecureRandom.urlsafe_base64,
-#         email: "test_wpman#{n}@test.com",
-#         name: "test女性#{n}",
-#         introduction: "よろしくお願いします",
-#         sex: 1,
-#         history: n % 4,
-#         active_area: n % 47,
-#         objective: n % 5,
-#         frequency: n % 4,
-#         heart: n % 3,
-#         traning_style: n % 4
-#         )
-#       customer.profile_image.attach(io: File.open("./db/fixtures/sample-woman#{n}.jpg"), filename: "sample-woman#{n}.jpg", content_type: 'image/jpg')
-#       customer.save!
-#   end
-# end
+for n in 1..10
+  ActiveRecord::Base.transaction do
+      customer = Customer.new(
+        password: SecureRandom.urlsafe_base64,
+        email: "test_wpman#{n}@test.com",
+        name: "test女性#{n}",
+        introduction: "よろしくお願いします",
+        sex: 1,
+        history: n % 4,
+        active_area: n % 47,
+        objective: n % 5,
+        frequency: n % 4,
+        heart: n % 3,
+        traning_style: n % 4
+        )
+      customer.profile_image.attach(io: File.open("./db/fixtures/sample-woman#{n}.jpg"), filename: "sample-woman#{n}.jpg", content_type: 'image/jpg')
+      customer.save!
+  end
+end
 
 #==========会員に紐付いた投稿=================
 
-# ActiveRecord::Base.transaction do
-#   Customer.all.each_with_index do |customer, i|
-#     post = customer.posts.new(
-#       title: 'タイトル',
-#       caption: 'テキストテキストテキストテキストテキストテキストテキストテキストテキスト'
-#                   )
-#     post.image.attach(io: File.open(Rails.root.join("./db/fixtures/sample-post#{i}.jpg")),
-#                   filename: 'sample-post1.jpg')
-#     break if i == 20
-#       post.save!
-#   end
-# end
+ActiveRecord::Base.transaction do
+  Customer.all.each_with_index do |customer, i|
+    post = customer.posts.new(
+      title: 'タイトル',
+      caption: 'テキストテキストテキストテキストテキストテキストテキストテキストテキスト'
+                  )
+    post.image.attach(io: File.open(Rails.root.join("./db/fixtures/sample-post#{i}.jpg")),
+                  filename: 'sample-post1.jpg')
+    break if i == 20
+      post.save!
+  end
+end
 
 
